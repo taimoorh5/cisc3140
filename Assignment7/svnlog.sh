1e816f1d5e366fd993dccd8d8b3f6e8809d29b19
taimoorh5 <taimoorh5@gmail.com>
2019-12-06 08:00:33 -0500

    update

--- Assignment7/svnlog.sh
+++
@@ -1,7 +0,0 @@
-svn checkout https://github.com/taimoorh5/cisc3140.git
-svn add --parents makefile/Main.java
-svn add --parents makefile/makefile
-svn commit -m "Adding makefile"
-make
-java Main
-svn commit -m "Svn HW"
